47ad61c9ecd76f6f1a2dbc1db7ade36b903d9f76
靳肖健 <jinxiaojian@youxin.com>
2019-04-18 16:57:38 +0800

    feat

---
+++ app/Greeter.js
@@ -0,0 +1,9 @@
+/*
+ * @Description:
+ * @Author: jinxiaojian
+ * @Email: jinxiaojian@youxin.com
+ * @LastEditors: 靳肖健
+ * @Date: 2019-04-18 16:54:51
+ * @LastEditTime: 2019-04-18 16:55:40
+ */
+var Greeter = 1;
---
+++ app/main.js
@@ -0,0 +1,9 @@
+/*
+ * @Description:
+ * @Author: jinxiaojian
+ * @Email: jinxiaojian@youxin.com
+ * @LastEditors: 靳肖健
+ * @Date: 2019-04-18 16:55:03
+ * @LastEditTime: 2019-04-18 16:55:37
+ */
+var main = 1;
