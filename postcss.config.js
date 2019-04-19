56ee7dfc1297ed4e8e38da4898375f9d17c20752
靳肖健 <jinxiaojian@youxin.com>
2019-04-19 14:58:33 +0800

    fix

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Description:
+ * @Author: jinxiaojian
+ * @Email: jinxiaojian@youxin.com
+ * @LastEditors: 靳肖健
+ * @Date: 2019-04-19 14:45:01
+ * @LastEditTime: 2019-04-19 14:45:10
+ */
+module.exports = {
+  plugins: [require("autoprefixer")],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,10 @@
  * @Email: jinxiaojian@youxin.com
  * @LastEditors: 靳肖健
  * @Date: 2019-04-18 17:32:54
- * @LastEditTime: 2019-04-19 12:38:25
+ * @LastEditTime: 2019-04-19 14:56:38
  */
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: __dirname + "/app/main.js",
   devtool: "eval-source-map",
@@ -43,8 +45,17 @@ module.exports = {
               localIdentName: "[name]__[local]--[hash:base64:5]",
             },
           },
+          {
+            loader: "postcss-loader",
+          },
         ],
       },
     ],
   },
+  plugins: [
+    new webpack.BannerPlugin("靳xiaojian，翻版不究"),
+    new HtmlWebpackPlugin({
+      template: __dirname + "/app/index.tmpl.html", //new 一个这个插件的实例，并传入相关的参数
+    }),
+  ],
 };
